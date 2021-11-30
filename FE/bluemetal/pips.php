b3d8d2625c518ab6357fc9bde7f9bd5f4f2ab10d
Caleb Mazalevskis <maikuolan@gmail.com>
2021-12-01 00:35:54 +0800

    Aesthetic patch.

--- FE/bluemetal/pips.php
+++ FE/bluemetal/pips.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Blue metal theme pips handler (last modified: 2020.10.26).
+ * This file: Blue metal theme pips handler (last modified: 2021.11.30).
  */
 
 /** Prevents execution from outside of phpMussel. */
@@ -16,6 +16,10 @@ if (!defined('phpMussel')) {
     die('[phpMussel] This should not be accessed directly.');
 }
 
-$phpMussel['FE']['PIP_Left'] = 'R0lGODlhCAAIAIABAH+UsP///yH5BAEKAAEALAAAAAAIAAgAAAINjH+ga6vJIEDh0UmzKQA7';
-$phpMussel['FE']['PIP_Right'] = 'R0lGODlhCAAIAIABAH+UsP///yH5BAEKAAEALAAAAAAIAAgAAAINjH+gmwvoUGBSSfOuKQA7';
 $phpMussel['FE']['PIP_Key'] = 'R0lGODlhBwAJAIABAH+UsP///yH5BAEKAAEALAAAAAAHAAkAAAINjH+gyaaAAkQrznRbKAA7';
+$phpMussel['FE']['PIP_Left'] = 'R0lGODlhCAAIAIABAH+UsP///yH5BAEKAAEALAAAAAAIAAgAAAINjH+ga6vJIEDh0UmzKQA7';
+$phpMussel['FE']['PIP_Left_Invalid'] = 'R0lGODlhCAASAKECAJkCAH+UsAAAAAAAACH5BAEKAAAALAAAAAAIABIAAAIXhH+ha6vJYEDg0UmzzhZ1yGGeuIEXqRUAOw==';
+$phpMussel['FE']['PIP_Left_Valid'] = 'R0lGODlhCAASAKECAAKZAH+UsAAAAAAAACH5BAEKAAIALAAAAAAIABIAAAIclH+ha6vJYEDi0Umz3hxk4FEhAh6ANxpnqnJHAQA7';
+$phpMussel['FE']['PIP_Right'] = 'R0lGODlhCAAIAIABAH+UsP///yH5BAEKAAEALAAAAAAIAAgAAAINjH+gmwvoUGBSSfOuKQA7';
+$phpMussel['FE']['PIP_Right_Invalid'] = 'R0lGODlhCAASAKECAJkCAH+UsAAAAAAAACH5BAEKAAAALAAAAAAIABIAAAIYhH+hmxvoEGBSSfOuvrZm+DVcuGHkdEoFADs=';
+$phpMussel['FE']['PIP_Right_Valid'] = 'R0lGODlhCAASAKECAAKZAH+UsAAAAAAAACH5BAEKAAIALAAAAAAIABIAAAIblH+hmxvokGBSSfOu3hxc4EkhAh7laIBoyh0FADs=';
