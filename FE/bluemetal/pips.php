ca58bb0dd435b5583a6bdf75e97708c645b26168
Caleb Mazalevskis <maikuolan@gmail.com>
2022-01-08 02:11:12 +0800

    Aesthetic patch.

--- FE/bluemetal/pips.php
+++ FE/bluemetal/pips.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Blue metal theme pips handler (last modified: 2021.11.30).
+ * This file: Blue metal theme pips handler (last modified: 2021.12.11).
  */
 
 /** Prevents execution from outside of phpMussel. */
@@ -17,9 +17,9 @@ if (!defined('phpMussel')) {
 }
 
 $phpMussel['FE']['PIP_Key'] = 'R0lGODlhBwAJAIABAH+UsP///yH5BAEKAAEALAAAAAAHAAkAAAINjH+gyaaAAkQrznRbKAA7';
-$phpMussel['FE']['PIP_Left'] = 'R0lGODlhCAAIAIABAH+UsP///yH5BAEKAAEALAAAAAAIAAgAAAINjH+ga6vJIEDh0UmzKQA7';
+$phpMussel['FE']['PIP_Left'] = 'R0lGODlhCAAQAIABAH+UsAAAACH5BAEKAAEALAAAAAAIABAAAAIWjH+ga6vJIEDh0Ulhtojri2Vb+IlGAQA7';
 $phpMussel['FE']['PIP_Left_Invalid'] = 'R0lGODlhCAASAKECAJkCAH+UsAAAAAAAACH5BAEKAAAALAAAAAAIABIAAAIXhH+ha6vJYEDg0UmzzhZ1yGGeuIEXqRUAOw==';
-$phpMussel['FE']['PIP_Left_Valid'] = 'R0lGODlhCAASAKECAAKZAH+UsAAAAAAAACH5BAEKAAIALAAAAAAIABIAAAIclH+ha6vJYEDi0Umz3hxk4FEhAh6ANxpnqnJHAQA7';
+$phpMussel['FE']['PIP_Left_Valid'] = 'R0lGODlhCAAQAKECAAKZAH+UsAAAAAAAACH5BAEKAAIALAAAAAAIABAAAAIalH+ha6vJYEDi0UmzFiCDToEIIJJcaIYbUgAAOw==';
 $phpMussel['FE']['PIP_Right'] = 'R0lGODlhCAAIAIABAH+UsP///yH5BAEKAAEALAAAAAAIAAgAAAINjH+gmwvoUGBSSfOuKQA7';
-$phpMussel['FE']['PIP_Right_Invalid'] = 'R0lGODlhCAASAKECAJkCAH+UsAAAAAAAACH5BAEKAAAALAAAAAAIABIAAAIYhH+hmxvoEGBSSfOuvrZm+DVcuGHkdEoFADs=';
-$phpMussel['FE']['PIP_Right_Valid'] = 'R0lGODlhCAASAKECAAKZAH+UsAAAAAAAACH5BAEKAAIALAAAAAAIABIAAAIblH+hmxvokGBSSfOu3hxc4EkhAh7laIBoyh0FADs=';
+$phpMussel['FE']['PIP_Right_Invalid'] = 'R0lGODlhCAAQAIABAH+UsAAAACH5BAEKAAEALAAAAAAIABAAAAIWjH+gmwvoUGBSSfPatTVDr10i94lIAQA7';
+$phpMussel['FE']['PIP_Right_Valid'] = 'R0lGODlhCAAQAKECAAKZAH+UsAAAAAAAACH5BAEKAAIALAAAAAAIABAAAAIalH+hmxvokGBSSfOuFuCCLoHId5Ci8Z3ohhQAOw==';
--- FE/fullmoon/pips.php
+++ FE/fullmoon/pips.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Full moon theme pips handler (last modified: 2021.11.30).
+ * This file: Full moon theme pips handler (last modified: 2021.12.11).
  */
 
 /** Prevents execution from outside of phpMussel. */
@@ -18,8 +18,8 @@ if (!defined('phpMussel')) {
 
 $phpMussel['FE']['PIP_Key'] = 'R0lGODlhBwAJAKECAMrMyf3/+////////yH5BAEKAAEALAAAAAAHAAkAAAINjH+gyaaAAkQrznRbKAA7';
 $phpMussel['FE']['PIP_Left'] = 'R0lGODlhCAAIAKECAMrMyfv99P///////yH5BAEKAAEALAAAAAAIAAgAAAINjH+ga6vJIEDh0UmzKQA7';
-$phpMussel['FE']['PIP_Left_Invalid'] = 'R0lGODlhCAASAKECAJkCAMrMyQAAAAAAACH5BAEKAAAALAAAAAAIABIAAAIXhH+ha6vJYEDg0UmzzhZ1yGGeuIEXqRUAOw==';
-$phpMussel['FE']['PIP_Left_Valid'] = 'R0lGODlhCAASAKECAAKZAMrMyQAAAAAAACH5BAEKAAIALAAAAAAIABIAAAIclH+ha6vJYEDi0Umz3hxk4FEhAh6ANxpnqnJHAQA7';
+$phpMussel['FE']['PIP_Left_Invalid'] = 'R0lGODlhCAAQAIABAMrMyQAAACH5BAEKAAEALAAAAAAIABAAAAIWjH+ga6vJIEDh0Ulhtojri2Vb+IlGAQA7';
+$phpMussel['FE']['PIP_Left_Valid'] = 'R0lGODlhCAAQAKECAAKZAMrMyQAAAAAAACH5BAEKAAIALAAAAAAIABAAAAIalH+ha6vJYEDi0UmzFiCDToEIIJJcaIYbUgAAOw==';
 $phpMussel['FE']['PIP_Right'] = 'R0lGODlhCAAIAKECAMrMyfv99P///////yH5BAEKAAEALAAAAAAIAAgAAAINjH+gmwvoUGBSSfOuKQA7';
-$phpMussel['FE']['PIP_Right_Invalid'] = 'R0lGODlhCAASAKECAJkCAMrMyQAAAAAAACH5BAEKAAAALAAAAAAIABIAAAIYhH+hmxvoEGBSSfOuvrZm+DVcuGHkdEoFADs=';
-$phpMussel['FE']['PIP_Right_Valid'] = 'R0lGODlhCAASAKECAAKZAMrMyQAAAAAAACH5BAEKAAIALAAAAAAIABIAAAIblH+hmxvokGBSSfOu3hxc4EkhAh7laIBoyh0FADs=';
+$phpMussel['FE']['PIP_Right_Invalid'] = 'R0lGODlhCAAQAIABAMrMyQAAACH5BAEKAAEALAAAAAAIABAAAAIWjH+gmwvoUGBSSfPatTVDr10i94lIAQA7';
+$phpMussel['FE']['PIP_Right_Valid'] = 'R0lGODlhCAAQAKECAAKZAMrMyQAAAAAAACH5BAEKAAIALAAAAAAIABAAAAIalH+hmxvokGBSSfOuFuCCLoHId5Ci8Z3ohhQAOw==';
